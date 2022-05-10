22719bf5ff17cab43697782fcd1bb496fd3e313e
SrishtiSingh2001 <2019346@iiitdmj.ac.in>
2022-05-10 13:48:04 +0530

    Speech Processing Completed

--- main.py
+++ main.py
@@ -2,16 +2,42 @@ import speech
 import cv2
 import os
 import datetime
+import detect
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= "dfkey.json"
-project_id = "blindbot-4f356"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= "drishti-tsrd-1b0f94978e8d.json"
+project_id = "drishti-tsrd"
 
 # create an object from speech module
 engine = speech.speech_to_text()
 
+listning = False
+
 print("Started....")
 while True:
-    print("Speak...")
-    resp = engine.recognize_speech_from_mic()
-    print("you said:", resp)
-    engine.text_speech(resp)
+
+    if not listning:
+        print("speak...")
+        resp = engine.recognize_speech_from_mic()
+        print("you said:", resp)
+
+        if resp != None:
+            intent, text = detect.detect_intent_texts(project_id, 0, [resp], 'en')
+            print("intent:", intent, "text:", text)
+        if(resp != None and intent == 'Drishti'):
+            listening = True
+    
+    else:
+        engine.text_speech("what can i help you with?")
+        intent = ''
+        engine.text_speech("Listening")
+        resp = engine.recognize_speech_from_mic()
+        print("you said:", resp)
+
+        engine.text_speech("Processing")
+        if(resp!=None):
+            print(resp)
+            intent, text = detect.detect_intent_texts(project_id, 0, [resp], 'en')
+            print("intent:", intent, "text:", text)
+
+        if resp is not None:
+            engine.text_speech(text)
