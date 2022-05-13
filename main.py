9c31e3fcebc56a4e29ac3456b64589eb034b28f6
Gargi Chaurasia <gargi.0901@gmail.com>
2022-05-13 08:54:45 +0530

    Object detection done

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
-import modules.speech as speech
 import cv2
 import os
 import datetime
+import modules.speech as speech
 import modules.detect as detect
 
 # setting up dialogflow credentials
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "drishti-srck-a65fae3e7146.json"
 DIALOGFLOW_PROJECT_ID = "drishti-srck"
 DIALOGFLOW_LANGUAGE_CODE = "en"
-
-# # Yolo paths for object detection
-# labelsPath = "yolo/coco.names"
-# weightsPath = "yolo/yolov3.weights"
-# configPath = "yolo/yolov3.cfg"
-# args = {"threshold": 0.3, "confidence": 0.5}
-#
-# # create an object of model used for object detection
-# model = yolopy.yolo(labelsPath, weightsPath, configPath)
 
 # create an object from speech module
 engine = speech.speech_to_text()
@@ -25,7 +16,7 @@ listening = False
 intent = None
 
 while True:
-    cam = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+    cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
     if not listening:
         resp = engine.recognize_speech_from_mic()
@@ -42,7 +33,6 @@ while True:
         engine.text_speech("Listening")
         resp = engine.recognize_speech_from_mic()
         engine.text_speech("Processing")
-        print("resp:", resp)
         if(resp != None):
             print(resp)
             intent, text = detect.detect_intent_texts(
@@ -53,9 +43,6 @@ while True:
             print(text)
             listening = False
             engine.text_speech(text)
-        elif intent == 'Brightness':
-            engine.text_speech("It is {} outside".format(
-                (functions.getBrightness(cam))[0]))
         elif intent == "FillForm":
             detect.detect_form(cam, engine)
         elif intent == "Read":
@@ -69,5 +56,3 @@ while True:
             engine.text_speech(text)
 
     cam.release()
-
-# cv2.destroyAllWindows()
