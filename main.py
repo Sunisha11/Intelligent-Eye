ce418f17cb8652099d5a435a6641a2962bd34c65
SrishtiSingh2001 <2019346@iiitdmj.ac.in>
2022-05-18 00:15:15 +0530

    Detect Form Added

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ intent = None
 
 while True:
     cam = cv2.VideoCapture(0)
-    
+
     if not listening:
         resp = engine.recognize_speech_from_mic()
         print(resp)
@@ -36,22 +36,26 @@ while True:
         engine.text_speech("Processing")
         if(resp != None):
             print(resp)
-            intent, text = detect.detect_intent_texts(DIALOGFLOW_PROJECT_ID, 0, [resp], DIALOGFLOW_LANGUAGE_CODE)
+            intent, text = detect.detect_intent_texts(
+                DIALOGFLOW_PROJECT_ID, 0, [resp], DIALOGFLOW_LANGUAGE_CODE)
         if intent == 'Describe':
             detect.describeScene(cam, engine)
         elif intent == 'Brightness':
-            engine.text_speech("It is {} outside".format((functions.getBrightness(cam))[0]))
+            engine.text_speech("It is {} outside".format(
+                (functions.getBrightness(cam))[0]))
         elif intent == "Time":
             currentDT = datetime.datetime.now()
-            engine.text_speech("The time is {} hours and {} minutes".format(currentDT.hour, currentDT.minute))
+            engine.text_speech("The time is {} hours and {} minutes".format(
+                currentDT.hour, currentDT.minute))
         elif intent == "Read":
-            print("read")
             detect.detect_text(cam, engine)
+        elif intent == "FillForm":
+            detect.detect_form(engine)
         elif intent == 'endconvo':
             print(text)
             listening = False
             engine.text_speech(text)
         elif resp != 'None':
             engine.text_speech(text)
-  
+
     cam.release()
