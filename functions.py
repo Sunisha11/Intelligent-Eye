7a8e98afa26819444a83925e091290fc76e51fc3
SrishtiSingh2001 <2019346@iiitdmj.ac.in>
2022-05-19 14:14:40 +0530

    Updated

--- functions.py
+++ functions.py
@@ -1,20 +1,28 @@
 import cv2
 import numpy as np
 import os
+import modules.speech as speech
+
+engine = speech.speech_to_text()
+
 
 def getBrightness(cam):
     ret, frame = cam.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    avg = np.sum(frame) / (frame.shape[0] * frame.shape[1])
-    avg = avg / 255
-    if(avg > 0.6):
-        return ("Very bright", avg)
-    if(avg > 0.4):
-        return ("Bright", avg)
-    if(avg >0.2):
-        return ("Dim", avg)
+    if ret == None:
+        engine.text_speech("Not getting any frame. Quitting now...")
     else:
-        return ("Dark",avg)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        avg = np.sum(frame) / (frame.shape[0] * frame.shape[1])
+        avg = avg / 255
+        if(avg > 0.6):
+            return ("Very bright", avg)
+        if(avg > 0.4):
+            return ("Bright", avg)
+        if(avg > 0.2):
+            return ("Dim", avg)
+        else:
+            return ("Dark", avg)
+
 
 def play_file(fname):
     return os.system("mpg123 " + fname)
--- modules/detect.py
+++ modules/detect.py
@@ -1,5 +1,4 @@
 import io
-from threading import local
 import cv2
 from google.oauth2 import service_account
 import google.cloud.vision_v1 as vision
@@ -16,28 +15,18 @@ def detect_intent_texts(project_id, session_id, texts, language_code):
     of the conversation."""
 
     session_client = dialogflow.SessionsClient()
-
     session = session_client.session_path(project_id, session_id)
-    # print('Session path: {}\n'.format(session))
 
     for text in texts:
         text_input = dialogflow.types.TextInput(
             text=text, language_code=language_code)
-
         query_input = dialogflow.types.QueryInput(text=text_input)
 
         try:
             response = session_client.detect_intent(
                 session=session, query_input=query_input)
-
         except InvalidArgument:
             raise
-
-        # print("Query text:", response.query_result.query_text)
-        # print("Detected intent:", response.query_result.intent.display_name)
-        # print("Detected intent confidence:",
-        #       response.query_result.intent_detection_confidence)
-        # print("Fulfillment text:", response.query_result.fulfillment_text)
 
     return response.query_result.intent.display_name, response.query_result.fulfillment_text
 
@@ -45,51 +34,53 @@ def detect_intent_texts(project_id, session_id, texts, language_code):
 def describeScene(cam, engine):
     print(__file__)
     ret, frame = cam.read()
-    cv2.imwrite('op.jpg', frame)
-    credentials = service_account.Credentials.from_service_account_file(
-        "united-monument-350013-0e8fdf2efe4a.json")
-    client = vision.ImageAnnotatorClient(credentials=credentials)
-    path = 'op.jpg'
-    # path = 'images/road2.jpg'
-    with io.open(path, 'rb') as image_file:
-        content = image_file.read()
-    image = vision.types.Image(content=content)
-    response = client.label_detection(image=image)
-    labels = response.label_annotations
+    if ret == None:
+        engine.text_speech("Not getting any frame. Quitting now...")
+    else:
+        cv2.imwrite('op.jpg', frame)
+        credentials = service_account.Credentials.from_service_account_file(
+            "united-monument-350013-0e8fdf2efe4a.json")
+        client = vision.ImageAnnotatorClient(credentials=credentials)
+        path = 'op.jpg'
+        # path = 'images/road2.jpg'
+        with io.open(path, 'rb') as image_file:
+            content = image_file.read()
+        image = vision.types.Image(content=content)
+        response = client.label_detection(image=image)
+        labels = response.label_annotations
 
-    engine.text_speech("Description of the view")
+        engine.text_speech("Description of the view")
 
-    tellObjects(client, image, engine)
+        tellObjects(client, image, engine)
 
-
-def tellObjects(client, image,  engine):
-    objects = client.object_localization(
-        image=image).localized_object_annotations
-    engine.text_speech("I will tell you the objects near you")
-    for object_ in objects:
-        engine.text_speech(object_.name)
-    lbldict = {}
-    for i in objects:
-        if i.name in lbldict:
-            lbldict[i.name] += 1
-        else:
-            lbldict[i.name] = 1
-    once = True
-    length = len(lbldict)
-    r = 0
-    for i, j in lbldict.items():
-        if once:
-            if j != 1:
-                engine.text_speech("There are")
+    def tellObjects(client, image,  engine):
+        objects = client.object_localization(
+            image=image).localized_object_annotations
+        engine.text_speech("I will tell you the objects near you")
+        for object_ in objects:
+            engine.text_speech(object_.name)
+        lbldict = {}
+        for i in objects:
+            if i.name in lbldict:
+                lbldict[i.name] += 1
             else:
-                engine.text_speech("There is")
-            once = False
-        engine.text_speech("{} {}".format(j, i))
-        r += 1
-        if r != length:
-            engine.text_speech("and")
-    if (length == 0):
-        engine.text_speech("No objects found")
+                lbldict[i.name] = 1
+        once = True
+        length = len(lbldict)
+        r = 0
+        for i, j in lbldict.items():
+            if once:
+                if j != 1:
+                    engine.text_speech("There are")
+                else:
+                    engine.text_speech("There is")
+                once = False
+            engine.text_speech("{} {}".format(j, i))
+            r += 1
+            if r != length:
+                engine.text_speech("and")
+        if (length == 0):
+            engine.text_speech("No objects found")
 
 
 def detect_text(cam, engine):
@@ -97,23 +88,26 @@ def detect_text(cam, engine):
         "united-monument-350013-0e8fdf2efe4a.json")
     client = vision.ImageAnnotatorClient(credentials=credentials)
     ret, content = cam.read()
-    cv2.imwrite('op.jpg', content)
-    path = 'op.jpg'
-    with io.open(path, 'rb') as image_file:
-        content = image_file.read()
-    image = vision.types.Image(content=content)
-    response = client.text_detection(image=image)
-    locale = response.text_annotations[0].locale
-    lang = languages.get(alpha2=locale).name
-    text = response.text_annotations[0].description
-    print("text:", text)
-    if locale == 'en':
-        engine.text_speech(
-            f'The text is written in {lang} and it goes like this...')
-        engine.text_speech(text)
+    if ret == None:
+        engine.text_speech("Not getting any frame. Quitting now...")
     else:
-        engine.text_speech(
-            f'Sorry, currently I am not trained enough to read text written in {lang}. Why not try reading something in English, Hindi or Tamil?')
+        cv2.imwrite('op.jpg', content)
+        path = 'op.jpg'
+        with io.open(path, 'rb') as image_file:
+            content = image_file.read()
+        image = vision.types.Image(content=content)
+        response = client.text_detection(image=image)
+        locale = response.text_annotations[0].locale
+        lang = languages.get(alpha2=locale).name
+        text = response.text_annotations[0].description
+        print("text:", text)
+        if locale == 'en':
+            engine.text_speech(
+                f'The text is written in {lang} and it goes like this...')
+            engine.text_speech(text)
+        else:
+            engine.text_speech(
+                f'Sorry, currently I am not trained enough to read text written in {lang}. Why not try reading something in English!?')
 
 
 def detect_form(engine):
@@ -136,4 +130,4 @@ def detect_form(engine):
         engine.text_speech(text)
     else:
         engine.text_speech(
-            f'Sorry, currently I am not trained enough to read text written in {lang}. Why not try reading something in English, Hindi or Tamil?')
+            f'Sorry, currently I am not trained enough to read text written in {lang}. Why not try reading something in English!?')
